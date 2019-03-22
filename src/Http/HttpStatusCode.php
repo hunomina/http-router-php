480197fdf37e0215bd21685a791f44a7dd582112
hunomina <hugo.f972@gmail.com>
2019-03-22 10:00:35 +0100

    Add some HTTP Status code and Fix Header Syntax

--- src/Http/HttpStatusCode.php
+++ src/Http/HttpStatusCode.php
@@ -4,11 +4,17 @@ namespace hunomina\Http;
 
 abstract class HttpStatusCode
 {
-    private const HTTP_PROTOCOL = 'HTTP 1/1';
+    private const HTTP_PROTOCOL = 'HTTP/1.1';
 
+    /**
+     * @link https://www.ietf.org/assignments/http-status-codes/http-status-codes.xml
+     */
     private const STATUS_CODE = [
         100 => 'Continue',
         101 => 'Switching Protocol',
+        102 => 'Processing',
+        103 => 'Early Hints',
+
         200 => 'OK',
         201 => 'Created',
         202 => 'Accepted',
@@ -16,6 +22,10 @@ abstract class HttpStatusCode
         204 => 'No Content',
         205 => 'Reset Content',
         206 => 'Partial Content',
+        207 => 'Multiple Status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
+
         300 => 'Multiple Choices',
         301 => 'Moved Permanently',
         302 => 'Found',
@@ -23,8 +33,10 @@ abstract class HttpStatusCode
         304 => 'Not Modified',
         307 => 'Temporary Redirect',
         308 => 'Permanent Redirect',
+
         400 => 'Bad Request',
         401 => 'Unauthorized',
+        402 => 'Payment Required',
         403 => 'Forbidden',
         404 => 'Not Found',
         405 => 'Method Not Allowed',
@@ -40,20 +52,28 @@ abstract class HttpStatusCode
         415 => 'Unsupported Media Type',
         416 => 'Range Not Satisfiable',
         417 => 'Expectation Failed',
-        418 => 'I\'m a teapot',
+        421 => 'Misdirected Request',
         422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependencies',
         425 => 'Too Early',
         426 => 'Upgrade Required',
         428 => 'Precondition Required',
         429 => 'Too Many Requests',
         431 => 'Request Header Fields Too Large',
         451 => 'Unavailable For Legal Reasons',
+
         500 => 'Internal Server Error',
         501 => 'Not Implemented',
         502 => 'Bad Gateway',
         503 => 'Service Unavailable',
         504 => 'Gateway Timeout',
         505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        509 => 'Unassigned',
+        510 => 'Not Extended',
         511 => 'Network Authentication Required'
     ];
 
